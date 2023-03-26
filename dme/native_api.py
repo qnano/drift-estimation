e4f57719edf4c4eadc79539c6d41f25bcd36728a
Jelmer <j.p.cnossen@tudelft.nl>
2023-03-26 20:55:56 +0200

    fix lib path on linux

--- dme/native_api.py
+++ dme/native_api.py
@@ -35,7 +35,7 @@ class NativeAPI:
         if os.name == 'nt':
             dllpath = f"/../bin/{dllpath}{name}.dll"
         else:
-            dllpath = f"/../bin/{dllpath}lib{name}.so"
+            dllpath = f"/bin/{dllpath}lib{name}.so"
 
         abs_dllpath = os.path.abspath(thispath + dllpath)
         
